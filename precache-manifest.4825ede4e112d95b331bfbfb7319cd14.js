f03c1f0709cad266e1ac29d3d832cd7af3781512
dmytsv <dmitrytsuprunov@Dmitrys-Mini.home>
2019-04-22 01:53:28 -0400

    Updates

---
+++ precache-manifest.4825ede4e112d95b331bfbfb7319cd14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74432c7ec4d92bf4bad8",
+    "url": "/event-app/static/js/runtime~main.0a7e81ba.js"
+  },
+  {
+    "revision": "268f704b82cc625e1f14",
+    "url": "/event-app/static/js/main.4771591b.chunk.js"
+  },
+  {
+    "revision": "fd12533bd418cc44ebe9",
+    "url": "/event-app/static/js/2.0833c0e2.chunk.js"
+  },
+  {
+    "revision": "268f704b82cc625e1f14",
+    "url": "/event-app/static/css/main.663b67dc.chunk.css"
+  },
+  {
+    "revision": "4dec5c344d4649c8372615ffce5acb05",
+    "url": "/event-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dmytsv/event-app/precache-manifest.134d00da66574f52fd2e01269129ac21.js"
+  "/event-app/precache-manifest.4825ede4e112d95b331bfbfb7319cd14.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dmytsv/event-app/index.html", {
+workbox.routing.registerNavigationRoute("/event-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
