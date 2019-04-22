8db660e283e9cabc80d979112d95b3dba3d3e5c6
dmytsv <dmitrytsuprunov@Dmitrys-Mini.home>
2019-04-22 01:48:37 -0400

    Updates

---
+++ precache-manifest.134d00da66574f52fd2e01269129ac21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9429ecf90a90b67552b",
+    "url": "/dmytsv/event-app/static/js/runtime~main.1f91d071.js"
+  },
+  {
+    "revision": "268f704b82cc625e1f14",
+    "url": "/dmytsv/event-app/static/js/main.4771591b.chunk.js"
+  },
+  {
+    "revision": "fd12533bd418cc44ebe9",
+    "url": "/dmytsv/event-app/static/js/2.0833c0e2.chunk.js"
+  },
+  {
+    "revision": "268f704b82cc625e1f14",
+    "url": "/dmytsv/event-app/static/css/main.663b67dc.chunk.css"
+  },
+  {
+    "revision": "fd03a90da86a54d51a514c5994c11774",
+    "url": "/dmytsv/event-app/index.html"
+  }
+];
